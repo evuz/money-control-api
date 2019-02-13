b985b49d94815fbbb91309e8dce970e3e9b9759b
evuz <jesus_jgb_45@hotmail.com>
2019-02-13 15:42:28 +0100

    chore: update domain v0.1.1

--- src/activity/activities.controller.ts
+++ src/activity/activities.controller.ts
@@ -3,14 +3,12 @@ import { Activity } from 'money-control-domain';
 
 import { domain } from '../domain';
 
-@Controller('activities/:userId/:date')
+@Controller('activities/:user/:date')
 export class ActivitiesController {
   @Get()
-  async getActivity(@Param() { userId, date }, @Query() { take, page }): Promise<Activity[]> {
+  async getActivity(@Param() { user, date }, @Query() { take, page }): Promise<Activity[]> {
     const useCase = take ? 'get_activities_by_month_paginated' : 'get_activities_by_month';
-    const activities = await domain
-      .get({ useCase })
-      .execute({ userId: +userId, date: +date, take: +take, page: +page });
+    const activities = await domain.get({ useCase }).execute({ user, date: +date, take: +take, page: +page });
     return activities.results;
   }
 }
--- src/activity/activity.controller.ts
+++ src/activity/activity.controller.ts
@@ -2,14 +2,14 @@ import { Get, Controller, Param, Delete } from '@nestjs/common';
 import { domain } from '../domain';
 import { Activity } from 'money-control-domain';
 
-@Controller('activity/:id')
+@Controller('activity')
 export class ActivityController {
-  @Get()
+  @Get(':id')
   async getActivity(@Param() { id }): Promise<Activity> {
     return await domain.get({ useCase: 'get_activity' }).execute({ id });
   }
 
-  @Delete()
+  @Delete(':id')
   async deleteActivity(@Param() { id }) {
     return domain.get({ useCase: 'remove_activity' }).execute({ id });
   }
