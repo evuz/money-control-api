6ef8a14995ea07657b48c39b08a0c2abebabc190
evuz <jesus_jgb_45@hotmail.com>
2019-02-14 17:34:41 +0100

    fix: getActivities return results and total

--- src/activity/activities.controller.ts
+++ src/activity/activities.controller.ts
@@ -2,13 +2,13 @@ import { Get, Controller, Param, Query } from '@nestjs/common';
 import { Activity } from 'money-control-domain';
 
 import { domain } from '../domain';
+import { IGetActivities } from 'money-control-domain/build/activity/Repositories/ActivityRepository';
 
 @Controller('activities/:user/:date')
 export class ActivitiesController {
   @Get()
-  async getActivity(@Param() { user, date }, @Query() { take, page }): Promise<Activity[]> {
+  async getActivities(@Param() { user, date }, @Query() { take, page }): Promise<IGetActivities> {
     const useCase = take ? 'get_activities_by_month_paginated' : 'get_activities_by_month';
-    const activities = await domain.get({ useCase }).execute({ user, date: +date, take: +take, page: +page });
-    return activities.results;
+    return domain.get({ useCase }).execute({ user, date: +date, take: +take, page: +page });
   }
 }
