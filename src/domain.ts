aa0c6ec8722a9c20e7caa3dfe23fcb9cde0517f1
evuz <jesus_jgb_45@hotmail.com>
2019-02-16 09:34:21 +0100

    feat: update domain v0.1.4

--- src/domain.ts
+++ src/domain.ts
@@ -1,15 +1,23 @@
 import { Domain } from 'ts-domain';
-import { DatabaseUseCaseFactory, UsersUseCaseFactory, ActivityUseCaseFactory } from 'money-control-domain';
+import {
+  DatabaseUseCaseFactory,
+  UsersUseCaseFactory,
+  ActivityUseCaseFactory,
+  repositorySelector,
+} from 'money-control-domain';
 
+const config = {};
+
+repositorySelector.select({ repository: 'mongo' });
 const useCases = {
-  start_database: DatabaseUseCaseFactory.startDatabaseUseCase(),
-  get_activity: ActivityUseCaseFactory.getActivityUseCase(),
-  get_activities_by_month: ActivityUseCaseFactory.getActivitiesByMonthUseCase(),
-  get_activities_by_month_paginated: ActivityUseCaseFactory.getActivitiesByMonthPaginatedUseCase(),
-  remove_activity: ActivityUseCaseFactory.removeActivityUseCase(),
-  new_income: ActivityUseCaseFactory.newIncomeUseCase(),
-  new_expense: ActivityUseCaseFactory.newExpenseUseCase(),
-  get_user_by_telegram_id: UsersUseCaseFactory.getUserByTelegramIdUseCase(),
+  start_database: DatabaseUseCaseFactory.startDatabaseUseCase({ config }),
+  get_activity: ActivityUseCaseFactory.getActivityUseCase({ config }),
+  get_activities_by_month: ActivityUseCaseFactory.getActivitiesByMonthUseCase({ config }),
+  get_activities_by_month_paginated: ActivityUseCaseFactory.getActivitiesByMonthPaginatedUseCase({ config }),
+  remove_activity: ActivityUseCaseFactory.removeActivityUseCase({ config }),
+  new_income: ActivityUseCaseFactory.newIncomeUseCase({ config }),
+  new_expense: ActivityUseCaseFactory.newExpenseUseCase({ config }),
+  get_user_by_telegram_id: UsersUseCaseFactory.getUserByTelegramIdUseCase({ config }),
 };
 
-export const domain = new Domain({ useCases });
+export const domain = new Domain({ useCases, config });
