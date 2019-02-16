8863216171c59786098f9e06bc3fe8af4d25a799
Jesus Gómez <jesus_jgb_45@hotmail.com>
2019-02-16 19:18:00 +0100

    perf: remove app controller

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,13 +1,9 @@
 import { Module } from '@nestjs/common';
 
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
 import { ActivityModule } from './activity/activity.module';
 import { UserModule } from './user/user.module';
 
 @Module({
   imports: [ActivityModule, UserModule],
-  controllers: [AppController],
-  providers: [AppService],
 })
 export class AppModule {}
